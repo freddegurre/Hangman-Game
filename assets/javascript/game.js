9bef39933011c9b0486bee01099ba2e49b47c551
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-02-13 15:08:45 -0800

    store user guess input in arrey userGuess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,18 @@
 //Words to chose from
 var words = ["elephan", "banana", "zebra", "pig"];
 //What letter user guesses
-var userInput = [];
+var userGuess = [];
 
 //computer picks a random word
 var computerChoise = words[Math.floor(Math.random() * words.length)];
-
 console.log(computerChoise)
 
+//store user guess in arrey userInput
+document.onkeyup = function(event) {
+    userGuess.push(event.key)
+    console.log(userGuess)
+  };
+
+ 
 
 
