9a13eeaf7017abff33850163ef52ada25d864cd9
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-02-13 14:54:47 -0800

    added words for game & computer choses random word.

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+
+//Words to chose from
+var words = ["elephan", "banana", "zebra", "pig"];
+//What letter user guesses
+var userInput = [];
+
+//computer picks a random word
+var computerChoise = words[Math.floor(Math.random() * words.length)];
+
+console.log(computerChoise)
+
+
+
