a309dbcdad4c38916e48a29b760edddebac88a16
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-02-13 19:20:31 -0800

    added var wordLength to store number of length in chosen word so that i can print _ on screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,35 @@ var words = ["elephan", "banana", "zebra", "pig"];
 //What letter user guesses
 var userGuess = [];
 
+
 //computer picks a random word
 var computerChoise = words[Math.floor(Math.random() * words.length)];
-console.log(computerChoise)
+var wordLength = computerChoise.length;
 
-//store user guess in arrey userGuess and print the guess to screen :) 
+
+//store user guess in arrey userGuess and print the guess to screen not complete :) 
 document.onkeyup = function(event) {
+    if (userGuess.indexOf(userGuess) === -1 ){
     userGuess.push(event.key);
     userGuess.toString();
     document.getElementById("guess").innerHTML = userGuess;
     console.log(userGuess)
+    }
+
   };
+
+
+
+
+  /*for (var i = 0; i < userGuess.length; i++) {
+    if (userGuess[i] === ) 
+    {   
+       alert('Value exist');
+    }else{
+       alert('Value doesn't exist');
+    }
+
+console.log(words.indexOf("banana"))*/
 
 
 
