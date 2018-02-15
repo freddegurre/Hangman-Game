e4cf9ca063ce51bcdcaab1f4acc5489061155f3b
Fredrik Gustafson <Fredrik@macbooks-MacBook-Pro.local>
2018-02-14 16:15:08 -0800

    so so close, just need to replace _ with right guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,10 @@
 var words = ["elephan", "banana", "zebra", "pig"];
 //What letter user guesses
 var userGuess = [];
+//store wrong guesses
+var rightGuess = [];
+
+var wrongGuess = [];
 
 //computer picks a random word from words
 var computerChoise = words[Math.floor(Math.random() * words.length)];
@@ -13,33 +17,47 @@ var splitChoise = computerChoise.split("");
 //Calculate lenght of word and print it to screen
 //var wordLength = computerChoise.length;
 for (i = 0; i < computerChoise.length; i++ ) {
-    document.write(" _ ")
+    document.getElementById("word").textContent += (" _ ");
 }
 
 //store user guess in arrey userGuess and print the guess to screen 
 document.onkeyup = function(event) {
-    //If key.event does not match exsisting index in userGuess
-    if (userGuess.indexOf(event.key) === -1 ){
+
+  //If key.event does not match exsisting index in userGuess
+    if (userGuess.indexOf(event.key)  === -1 ){
         
         //then add key.event to userGuess
         userGuess.push(event.key);
 
         //convert user guess to string 
         userGuess.toString();
-
+    
+      if (splitChoise.indexOf(event.key) >=0) {
         //print the string to the HTML file as a letter that has been guessed
-        document.getElementById("guess").textContent = userGuess;
-    }
-    //does the userGuess = any of the strings in splitChoise? 
-    if (splitChoise.indexOf(event.key) >=0) {
-      //if true check index number of matching string in splitChoise array. 
-
+        rightGuess.push(event.key);
+      }
       
+      else {
+        wrongGuess.push(event.key);
+        document.getElementById("guess").textContent = wrongGuess;
+      }
     }
-
-  };
-
+  
+};
 console.log(splitChoise);
+   
+    //does the userGuess = any of the strings in splitChoise? 
+    /*else if (splitChoise.indexOf(event.key) >=0) {
+      //if true check index number of matching string in splitChoise array.
+      document.getElementById("word").textContent = event.key;
+  }*/
+
+
+  
+
+
+
+
 
 
   /*for (var i = 0; i < userGuess.length; i++) {
